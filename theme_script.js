f0df43cf34033c2ee685aa88b417b9c5a6fd90e9
Gilbert Alba <gilbert.alba1110@gmail.com>
2023-05-14 17:31:03 -0500

    commit 2

--- theme_script.js
+++ theme_script.js
@@ -26,3 +26,39 @@ function setThemeStyle(style){
     localStorage.setItem('savedStyle', style);
 }
 
+//Contact Form
+const form = document.querySelector("form");
+form.addEventListener("submit'", (event) => {
+    //prevent the form from refreshing the page
+    event.preventDefault();
+
+    const { name, subject, email, message } = event.target;
+    
+    //API Gateway API endpoint URL
+    const endpoint = "<https://atvc1mufqj.execute-api.us-east-2.amazonaws.com/default/sendContactEmail>";
+
+    const body = JSON.stringify({
+        senderName: name.value,
+        senderSubject: subject.value,
+        senderEmail: email.value,
+        message: message.value
+    });
+    const requestOptions = {
+        method: "POST",
+        body
+    };
+
+    fetch(endpoint, requestOptions)
+        .then((response) => {
+            if (!response.ok) throw new Error("Error in fetch");
+            return response.json();
+        })
+        .then((response) => {
+            document.getElementById("result-text").innerText = "Email sent successfully!";
+        })
+        .catch((error) => {
+            document.getElementById("result-text").innerText = "An unknown error occurred.";
+        });
+
+});
+
